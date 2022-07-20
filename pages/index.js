703f1c75990dd5bd32168325c8a3d63ca95cf56d
ThomasHeim11e <theim@MSI.localdomain>
2022-07-20 21:28:51 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Yer mom gay</h1>
+    </div>
+  );
+};
+
+export default Home;
